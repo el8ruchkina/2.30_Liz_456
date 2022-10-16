ed8bb4c4d98c8ce04b55294d9311ce5698206277
el8ruchkina <115945562+el8ruchkina@users.noreply.github.com>
2022-10-16 21:40:53 +0300

    Add files via upload

---
+++ 2.30_Liz/2.30_Liz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class mainclass
+{
+    public static void Main()
+    {
+        int x1, x0, a, b;
+        Console.WriteLine("Число x после преобразований равно 456");
+        x1 = 456;
+        b = 456 % 10;
+        x1 = 456 - b;
+        a = (x1 % 100)/10;
+        x0 = x1 - (a*10) + b*10 + a;
+        Console.WriteLine($"Иcходный x равен {x0}");
+    }
+}
